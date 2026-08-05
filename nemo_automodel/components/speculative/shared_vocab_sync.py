53c8cb2e808b86f4e498380c9f5c673b9948b035
Nagarajan Karthik <nkarthik3092@gmail.com>
2026-08-05 05:04:44 +0000

    Added functions to discover embedding and lm_head.

--- nemo_automodel/components/speculative/shared_vocab_sync.py
+++ nemo_automodel/components/speculative/shared_vocab_sync.py
@@ -1,4 +1,7 @@
 from dataclasses import dataclass
+import torch
+import torch.distributed as dist
+from nemo_automodel.components.distributed.mesh_utils import get_flat_mesh
 
 
 
@@ -21,11 +24,19 @@ class SharedVocabSync:
         self.cfg = cfg
         self.model_parts = model_parts
         self.mesh_context = mesh_context
+        self.device_mesh = mesh_context.device_mesh
+        self.pp_mesh = get_flat_mesh(self.device_mesh, "pp")
         self.draft_model = draft_model
-        self._has_embedding = self._holds_embedding()
-        self._has_lm_head = self._holds_lm_head()
+        self._update_embedding = False
+        self._update_lm_head = False
+        self._update_embedding_adapters = False
+        self._update_lm_head_adapters = False
+        self.embedding = None
+        self.lm_head = None
+        self._discover_embedding()
+        self._discover_lm_head()
 
-    def _holds_embedding(self):
+    def _discover_embedding(self) -> None:
         """
         Check if any model part on the current rank 
         has an embedding layer.
@@ -33,10 +44,22 @@ class SharedVocabSync:
         for mp in self.model_parts:
             embedding = mp.get_input_embeddings()
             if embedding is not None:
-                return True
-        return False
+                has_embedding = True
+                self.embedding = embedding
+                # Embedding layers should not have lora adapters
+                # so we can just check if the embedding weight is trainable
+                if embedding.weight.requires_grad:
+                    self._update_embedding = True
+                break
+        dist.barrier()
+        has_embed = torch.tensor([int(has_embedding)], 
+                                 dtype=torch.int32, 
+                                 device=self.device_mesh.device)
+        pp_group = self.pp_mesh.get_group()
+        dist.all_reduce(has_embed, op=dist.ReduceOp.SUM, group=pp_group)
+        assert has_embed[0] == 1
 
-    def _holds_lm_head(self):
+    def _discover_lm_head(self) -> None:
         """
         Check if any model part on the current rank 
         has an lm_head layer.
@@ -44,9 +67,22 @@ class SharedVocabSync:
         for mp in self.model_parts:
             lm_head = mp.get_output_embeddings()
             if lm_head is not None:
-                return True
-        return False
+                has_lm_head = True
+                self.lm_head = lm_head
+                if hasattr(lm_head, "lora_A"):
+                    self._update_lm_head_adapters = True
+                elif lm_head.weight.requires_grad:
+                    self._update_lm_head = True
+                break
+        dist.barrier()
+        has_lm_head = torch.tensor([int(has_lm_head)], 
+                                 dtype=torch.int32, 
+                                 device=self.device_mesh.device)
+        pp_group = self.pp_mesh.get_group()
+        dist.all_reduce(has_lm_head, op=dist.ReduceOp.SUM, group=pp_group)
+        assert has_embed[0] == 1
 
+    
     def capture(self):
         raise NotImplementedError
 
